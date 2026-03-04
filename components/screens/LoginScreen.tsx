7e24d1b6769ff9ed7774fb7ca94243a570e75ae5
sejalpujari <2111cs020501@mallareddyuniversity.ac.in>
2026-03-05 00:09:25 +0530

    fix type commits

--- components/screens/LoginScreen.tsx
+++ components/screens/LoginScreen.tsx
@@ -14,13 +14,14 @@ export default function LoginScreen({
   onForgotPassword,
   onSignup,
 }: LoginScreenProps) {
+  const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const handleLogin = async () => {
     try {
-      const res = await fetch("http://127.0.0.1:8000/api/v1/auth/login", {
+      const res = await fetch(`${API_URL}/api/v1/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
